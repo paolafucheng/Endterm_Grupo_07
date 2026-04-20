ee9e647456918b0cea15e392de71dd0121235f4e
daburu <jdrodriguez@fp.lasalle.es>
2026-04-20 23:06:11 +0530

    feat: He implementado el método acelerar en la claase Vehiculo. Ahora debería de pasar el test

--- src/main/java/Vehiculo.java
+++ src/main/java/Vehiculo.java
@@ -1,4 +1,20 @@
-
 public class Vehiculo {
-    // Los atributos los definiremos tras el debate del README
+    private String marca;
+    private String modelo;
+    private int velocidadMaxima;
+    private int velocidadActual = 0;
+
+    public Vehiculo(String marca, String modelo, int velocidadMaxima) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public void acelerar(int incremento) {
+        this.velocidadActual += incremento;
+    }
+
+    public int getVelocidadActual() {
+        return velocidadActual;
+    }
 }
